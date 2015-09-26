93c5d6872dec5cc856cde62d39bcfcccd10bc499
albert925 <albertarias925@gmail.com>
2015-09-26 12:20:49 -0500

    exptrs

--- routers/admin.js
+++ routers/admin.js
@@ -1,6 +1,6 @@
 var bodyParser=require("body-parser");
 var cookieParser=require("cookie-parser");
-var session=require("express-session");
+//var session=require("express-session");
 var mysql=require("mysql");
 
 var conexion=mysql.createPool({//conexion multiple
@@ -25,9 +25,9 @@ exports.validacion=function (pet,res) {
 					//res.render("conaxadm/indexb",{a:"Encontrado"});
 					var admin=[{idad:result[0].id_adm,nmad:result[0].nam_adm,crad:result[0].cor_adm,tpad:result[0].tip_adm}];
 					//res.render("conaxadm/admin",{ad:admin});
-					var sess=pet.session;
-					sess.adm=result[0].id_adm;
-					sess.datadm={nmad:result[0].nam_adm,crad:result[0].cor_adm,tpad:result[0].tip_adm}
+					pet.session.adm=result[0].id_adm;
+					pet.session.datadm={nmad:result[0].nam_adm,crad:result[0].cor_adm,tpad:result[0].tip_adm}
+					console.log(pet.session.adm);
 					res.redirect("/conaxadm/administrador");
 				}
 				else{
@@ -38,42 +38,11 @@ exports.validacion=function (pet,res) {
 	});
 }
 exports.bienvenida=function (pet,res) {
+	console.log(pet.session.adm);
 	if (pet.session.adm) {
 		res.render("conaxadm/admin",{ad:pet.session.adm});
 	}
 	else{
-		res.redirect("conaxadm/index",{a:"Sesión caducada"});
+		res.redirect("/conaxadm");
 	}
 }
-/*router.post("/conaxadm/adm",function (pet,res,next) {
-	console.log(pet.body);
-	var usR=pet.body.usadm;
-	var psR=pet.body.pasadm;
-	var regs="SELECT * from administrador where nam_adm='"+usR+"' and pass_adm='"+psR+"'";
-	conexion.getConnection(function (error,conectT) {
-		conectT.query(regs,function (err,result) {
-			if (err) {
-				console.log(err);
-			}
-			else{
-				if (result.length>0) {
-					//res.render("conaxadm/indexb",{a:"Encontrado"});
-					var admin=[{idad:result[0].id_adm,nmad:result[0].nam_adm,crad:result[0].cor_adm,tpad:result[0].tip_adm}];
-					//res.render("conaxadm/admin",{ad:admin});
-					var sess=pet.session;
-					sess.adm=result[0].id_adm;
-					sess.datadm={nmad:result[0].nam_adm,crad:result[0].cor_adm,tpad:result[0].tip_adm}
-					if (sess.adm) {
-						res.render("conaxadm/admin",{ad:sess.adm});
-					}
-					else{
-						res.redirect("/conaxadm");
-					}
-				}
-				else{
-					res.render("conaxadm/index",{a:"Usuario o contraseña incorrectos"});
-				}
-			}
-		});
-	});
-});*/
